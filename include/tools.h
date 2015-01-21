316483034f50d880c2b7f691e9d7d3362ce8b1e7
dario-roselli <dario.roselli.1@gmail.com>
2015-01-21 19:07:51 +0100

    Include files
    added Include files

---
+++ include/tools.h
@@ -0,0 +1,24 @@
+/*
+ * tools.h
+ *
+ * Version: 1.0
+ * Date:	18.10.2013
+ *
+ * Copyright (c) 2013, jkuhlm - All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+#ifndef _TOOLS_H
+#define _TOOLS_H
+
+/****************************************************************
+ * Prototypes
+ ****************************************************************/
+int calcsum(int a, int b);
+
+#endif /* _TOOLS_H */
